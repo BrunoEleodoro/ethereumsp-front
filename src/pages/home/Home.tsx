9f5fed71bee7579cb293b81610f3268953531105
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2022-09-10 11:18:50 -0300

    home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,102 @@
-import { useEtherBalance, useEthers } from '@usedapp/core';
-import { BigNumber } from 'ethers';
-import { formatEther, parseEther } from 'ethers/lib/utils';
-import * as React from 'react';
+import { createStyles, Container, Text, Button, Group } from '@mantine/core';
+import { GithubIcon } from '@mantine/ds';
 
-export default function Home({ }) {
-  const { account } = useEthers();
-  const balance = useEtherBalance(account);
+const BREAKPOINT = '@media (max-width: 755px)';
 
-  return <>{account ? "Conta: " + account : ""}<br />
-    Balance: {balance ? formatEther(balance!.toString()) : null}
-  </>;
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    position: 'relative',
+    boxSizing: 'border-box',
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  },
+
+  inner: {
+    position: 'relative',
+    paddingTop: 0,
+    paddingBottom: 120,
+
+    [BREAKPOINT]: {
+      paddingBottom: 80,
+      paddingTop: 80,
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: 62,
+    fontWeight: 900,
+    lineHeight: 1.1,
+    margin: 0,
+    padding: 0,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+
+    [BREAKPOINT]: {
+      fontSize: 42,
+      lineHeight: 1.2,
+    },
+  },
+
+  description: {
+    marginTop: theme.spacing.sm,
+    fontSize: 24,
+
+    [BREAKPOINT]: {
+      fontSize: 18,
+    },
+  },
+
+  controls: {
+    marginTop: theme.spacing.xl * 2,
+
+    [BREAKPOINT]: {
+      marginTop: theme.spacing.xl,
+    },
+  },
+
+  control: {
+    height: 54,
+    paddingLeft: 38,
+    paddingRight: 38,
+
+    [BREAKPOINT]: {
+      height: 54,
+      paddingLeft: 18,
+      paddingRight: 18,
+      flex: 1,
+    },
+  },
+}));
+
+export default function Home() {
+  const { classes } = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <Container size={700} className={classes.inner}>
+        <h1 className={classes.title}>
+          O maior{' '}
+          <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
+            Agregador de NFTs
+          </Text>{' '} Focado em artistas
+        </h1>
+
+        <Text className={classes.description} color="dimmed">
+          Build fully functional accessible web applications with ease – Mantine includes more than
+          100 customizable components and hooks to cover you in any situation
+        </Text>
+
+        <Group className={classes.controls}>
+          <Button
+            size="xl"
+            className={classes.control}
+            variant="gradient"
+            gradient={{ from: 'blue', to: 'cyan' }}
+          >
+            Get started
+          </Button>
+
+        </Group>
+      </Container>
+    </div>
+  );
 }
